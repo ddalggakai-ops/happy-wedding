a244a45331fdfa536e4ba264e820d99aa6c9a476
Hyunjin <ttykopi@gmail.com>
2026-09-12 18:47:14 +0900

    photo bingo section: pink lace background, prize badge, CTA pulse

--- commit-push.bat
+++ commit-push.bat
@@ -3,12 +3,10 @@ cd /d "%~dp0"
 echo.
 echo === commit and push ===
 echo.
-rem stale lock files block git - remove them first
 if exist ".git\HEAD.lock"  del /f /q ".git\HEAD.lock"
 if exist ".git\index.lock" del /f /q ".git\index.lock"
-echo.
 git add -A
-git -c user.name="Hyunjin" -c user.email="ttykopi@gmail.com" commit -m "shrink photos and audio for faster first load" --author="Hyunjin <ttykopi@gmail.com>"
+git -c user.name="Hyunjin" -c user.email="ttykopi@gmail.com" commit -m "photo bingo section: pink lace background, prize badge, CTA pulse" --author="Hyunjin <ttykopi@gmail.com>"
 git push
 echo.
 echo ---- finished. press any key to close ----
